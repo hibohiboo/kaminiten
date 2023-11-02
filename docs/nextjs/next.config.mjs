925d46ac2b63e4586db621d77499ec2686df9226
hibohiboo <hibohiboo66+github@gmail.com>
2023-11-02 21:42:15 +0900

    fix export #4

--- docs/nextjs/next.config.mjs
+++ docs/nextjs/next.config.mjs
@@ -14,7 +14,7 @@ const withMDX = nextMDX({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
 }
