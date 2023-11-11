88c26a06021a307730e67585c68bf4e4040e5312
hibohiboo <hibohiboo66+github@gmail.com>
2023-11-12 02:04:44 +0900

    add file読み込み

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-
-import './App.css';
+import { Button } from '@blueprintjs/core';
+import { atom, useAtom } from 'jotai';
+declare global {
+  interface Window {
+    showDirectoryPicker(): Promise<FileSystemDirectoryHandle>;
+  }
+}
+interface FileSystemDirectoryHandle {
+  values(): AsyncIterableIterator<FileSystemFileHandle>;
+}
+const objAtom = atom({});
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [text, setText] = useAtom(objAtom);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Button
+        onClick={async () => {
+          // FileSystemAPIを使って選択したフォルダ内に含まれるファイル一覧を取得
+          let handle: FileSystemDirectoryHandle;
+          try {
+            handle = await window.showDirectoryPicker();
+            console.log(handle);
+            if (!handle) return;
+          } catch (e) {
+            console.log(e);
+            return;
+          }
+          const files = await handle.values();
+          for await (const file of files) {
+            // ファイル名をキーにしてオブジェクトに追加
+            setText((prev) => ({ ...prev, [file.name]: file.kind }));
+          }
+        }}
+      >
+        クリックで読み込み
+      </Button>
+      <pre>{JSON.stringify(text, null, 2)}</pre>
+    </div>
   );
 }
 
