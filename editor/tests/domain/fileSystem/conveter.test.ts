b803a9871d9bc4c6c6a294792f975af6b5e4cc87
hibohiboo <hibohiboo66+github@gmail.com>
2023-11-12 22:12:46 +0900

    add converter test #8

--- editor/tests/domain/fileSystem/conveter.test.ts
+++ editor/tests/domain/fileSystem/conveter.test.ts
@@ -22,12 +22,45 @@ describe('fileNodeStateToTreeNodeInfo', () => {
       nodeData: state,
     });
   });
-  it('ディレクトリが開いている場合、iconがfolder-openになる', () => {
-    const result = fileNodeStateToTreeNodeInfo({ ...state, isExpanded: true });
-    expect(result.icon).toBe('folder-open');
+  describe('hasCaretがリーフではfalseになること', () => {
+    it('kindがdirectoryの場合、hasCaretがtrueになる', () => {
+      const result = fileNodeStateToTreeNodeInfo(state);
+      expect(result.hasCaret).toBe(true);
+    });
+    it('kindがfileの場合、hasCaretがfalseになる', () => {
+      const result = fileNodeStateToTreeNodeInfo({
+        ...state,
+        kind: 'file',
+      });
+      expect(result.hasCaret).toBe(false);
+    });
   });
-  it('ファイルの場合、iconがdocumentになる', () => {
-    const result = fileNodeStateToTreeNodeInfo({ ...state, kind: 'file' });
-    expect(result.icon).toBe('document');
+  describe('iconの設定が正しいこと', () => {
+    it('ディレクトリが閉じている場合、iconがfolder-closeになる', () => {
+      const result = fileNodeStateToTreeNodeInfo(state);
+      expect(result.icon).toBe('folder-close');
+    });
+
+    it('ディレクトリが開いている場合、iconがfolder-openになる', () => {
+      const result = fileNodeStateToTreeNodeInfo({
+        ...state,
+        isExpanded: true,
+      });
+      expect(result.icon).toBe('folder-open');
+    });
+    it('ファイルの場合、iconがdocumentになる', () => {
+      const result = fileNodeStateToTreeNodeInfo({ ...state, kind: 'file' });
+      expect(result.icon).toBe('document');
+    });
+  });
+  describe('子要素が存在する場合、再帰的に解釈される', () => {
+    // 再帰的なテストは難しいので、子要素が存在する場合のみテストする
+    it('子要素が存在する場合、childNodeが存在する', () => {
+      const result = fileNodeStateToTreeNodeInfo({
+        ...state,
+        children: [state],
+      });
+      expect(result.childNodes).toHaveLength(1);
+    });
   });
 });
