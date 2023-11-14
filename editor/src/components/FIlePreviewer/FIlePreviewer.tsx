2b8122c2d7da5c529e3da06fae8027bcb3e88fcc
hibohiboo <hibohiboo66+github@gmail.com>
2023-11-14 21:18:39 +0900

    add file preview #8

--- editor/src/components/FIlePreviewer/FIlePreviewer.tsx
+++ editor/src/components/FIlePreviewer/FIlePreviewer.tsx
@@ -1,19 +1,52 @@
+import { readDataUrl } from '@kaminiten-editor/domain/fileSystem/imageFile/fileReader';
+import { validImageFile } from '@kaminiten-editor/domain/fileSystem/imageFile/validate';
 import { fileAtom } from '@kaminiten-editor/domain/selectFile/fileAtom';
 import { useAtom } from 'jotai';
 import { useEffect, useState } from 'react';
 
 function FIlePreviewer() {
   const [text, setText] = useState('');
+  const [type, setType] = useState('');
   const [file] = useAtom(fileAtom);
   useEffect(() => {
     (async function read() {
-      if (file) {
+      if (!file) {
+        setType('');
+        return;
+      }
+      if (
+        file.type.startsWith('text/') ||
+        file.type === 'application/json' ||
+        file.name.endsWith('.md')
+      ) {
         const text = await file.text();
         setText(text);
+        setType('text');
+        return;
       }
+      try {
+        if (file.type.startsWith('image/') && (await validImageFile(file))) {
+          const text = await readDataUrl(file);
+          setText(text);
+          setType('image');
+          return;
+        }
+      } catch (e) {
+        console.log(e);
+      }
+      setType('unknown');
     })();
   });
-  return <div>{text}</div>;
+  if (type === 'text') {
+    return <pre>{text}</pre>;
+  }
+  if (type === 'image') {
+    return <img src={text} alt="preview" />;
+  }
+  if (type === 'unknown') {
+    return <div>プレビュー未対応のファイル形式です。{file?.type}</div>;
+  }
+  return <></>;
 }
 
 export default FIlePreviewer;
